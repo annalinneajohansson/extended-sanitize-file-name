3258423525d9f09db07cfefa9169db997180ca60
Anna <vintagemaniac@gmail.com>
2015-04-17 11:58:26 +0200

    Update sanitize-filenames.php

--- sanitize-filenames.php
+++ sanitize-filenames.php
@@ -5,7 +5,7 @@ Plugin URI:
 Description: Replace and/or remove accents and other special characters in filenames on upload
 Version: 1
 Author: Anna Johansson
-Author URI: http://annathewebdesigner.com
+Author URI: http://annalinneajohansson.com
 */
 add_filter( 'sanitize_file_name', 'extended_sanitize_file_name', 10, 2 );
 function extended_sanitize_file_name( $filename ) {
